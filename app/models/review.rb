99150b14128f289decde75b3b3d6c99c7db42f58
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-05-26 12:06:44 +0100

    validations created

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { greater_than: -1, less_than: 6 }
+end
